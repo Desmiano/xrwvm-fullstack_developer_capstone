53c080e66c6dd717e06fae643c82c8c406e92c76
Desmiano <122176499+Desmiano@users.noreply.github.com>
2024-10-24 18:46:11 -0600

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ['localhost',
-               'https://dariansousa-8000.theiadockernext-1-labs-prod-'
-               'theiak8s-4-tor01.proxy.cognitiveclass.ai']
+                 'https://dariansousa-8000.theiadockernext-1-labs-prod-'
+                 'theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://dariansousa-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
